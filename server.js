3006060dca8f4657f0f6b96fc1b949f00e0f3f13
Keerti Chaudhary <17keerti@gmail.com>
2022-04-27 15:56:05 -0700

    change port for heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { json } = require('express/lib/response');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('./helpers/uuid');
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 

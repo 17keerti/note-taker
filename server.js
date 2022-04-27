307b6e530dd7b65f63a7f9e1575f2179dc4cdc59
Keerti Chaudhary <17keerti@gmail.com>
2022-04-27 15:42:12 -0700

    add app.get to take to index.html for any wrong path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const PORT = 3001;
 
 const app = express();
@@ -7,6 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)

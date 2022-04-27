6ad2e16a84a2a7027f1cee6980b49db271f6d8c1
Keerti Chaudhary <17keerti@gmail.com>
2022-04-27 15:48:21 -0700

    add function to write and save notes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { json } = require('express/lib/response');
+const fs = require('fs');
 const path = require('path');
+const uuid = require('./helpers/uuid');
 const PORT = 3001;
 
 const app = express();
@@ -13,10 +16,59 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(JSON.parse(data));
+      res.json(JSON.parse(data));
+    }
+  })
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting review');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
